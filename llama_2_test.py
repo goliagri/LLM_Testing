533a14a3dd3688322068326829ccea4db710bbff
Griffin Golias <goliagri@uw.edu>
2024-01-15 14:08:18 -0800

    merge

--- llama_2_test.py
+++ llama_2_test.py
@@ -7,31 +7,34 @@
 from transformers import AutoTokenizer
 import transformers
 import torch
+from time import time
 
 model = "meta-llama/Llama-2-7b-chat-hf"
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 def main():
+    start = time()
     tokenizer = AutoTokenizer.from_pretrained(model)
     pipeline = transformers.pipeline(
         "text-generation",
         model=model,
-        torch_dtype=torch.float32, #change to float16 for faster inference on gpu
+        torch_dtype=torch.float16,
         device_map="auto",
     )
 
     sequences = pipeline(
-        'I liked "Breaking Bad" and "Band of Brothers". Do you have any recommendations of other shows I might like?\n',
+        'they ',
         do_sample=True,
         top_k=10,
         num_return_sequences=1,
         eos_token_id=tokenizer.eos_token_id,
-        max_length=200,
+        max_length=100,
     )
     print(sequences)
     for seq in sequences:
         print(f"Result: {seq['generated_text']}")
-        print('!')
+    end = time()
+    print('Time taken: {0:.2f}'.format(int(end-start)))
 
 
 
